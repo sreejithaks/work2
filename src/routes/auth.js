973a2c841de3145a0511584a12cf0065068bb515
sreejithaks <sreejithaks112000@gmail.com>
2023-07-08 13:17:28 +0530

    first commit

---
+++ src/routes/auth.js
@@ -0,0 +1,14 @@
+const express=require('express');
+const router=express.Router();
+
+router.post("/register",(req,res)=>{
+    console.log(req.body);
+    res.send("register page");
+})
+
+router.post("/login",(req,res)=>{
+    console.log(req.body);
+    res.send("login page");
+})
+
+module.exports=router;
